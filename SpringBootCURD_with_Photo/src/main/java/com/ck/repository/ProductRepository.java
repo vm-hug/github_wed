180a675a6e3d1fef906aa1f39253d7e77d251386
vm-hug <minhhung191205@gmail.com>
2025-05-15 22:49:23 +0700

    15-5

--- SpringBootCURD_with_Photo/src/main/java/com/ck/repository/ProductRepository.java
+++ SpringBootCURD_with_Photo/src/main/java/com/ck/repository/ProductRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product , Long>{
-    // code written below for search product from data
+    
     @Query("SELECT product FROM Product product WHERE CONCAT(product.id,' ',product.productName,' ',product.price) LIKE %?1%")
     public List<Product> search(String keyword);
     public Product findByproductName(String productName);
